bc850b1eb25c3bc866fa1e5b9f11b7e876c18466
Olawale <adedekojoseph@gmail.com>
2019-04-10 17:55:23 +0100

    uodate

--- server/app.js
+++ server/app.js
@@ -9,12 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', (req, res) => {
-  res.status(200).json({
-    "status": 200,
-    "error": "WELCOME TO BANKA"
-  })
-})
+
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/accounts', validateToken, accountRouter);
 app.use('/api/v1/transactions', validateToken, transactionRouter);
